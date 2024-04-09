1e6128680958a15f6419aea3aa8ce31df4e0a7a8
duckhoi-UET <khoinguyen2000bg@gmail.com>
2024-04-10 00:04:53 +0700

    add input

---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input
+      type="text"
+      name="price"
+      id="price"
+      class="block w-full rounded-md border-0 py-2 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      placeholder="Name"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="w-full">
+  <div class="w-full max-w-[1200px] mx-auto">
+    <h1 class="text-2xl mb-4">Todo List</h1>
+    <div class="mb-4 flex items-center gap-4">
+      <Input />
+      <button
+        class="rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300"
+      >
+        Add
+      </button>
+    </div>
     <Table />
   </div>
 </template>
 
 <script>
 import Table from "../components/Table.vue";
+import Input from "../components/Input.vue";
 export default {
   components: {
     Table,
+    Input,
   },
   data() {
     return {
