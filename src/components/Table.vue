942f4c94d241e5b44f5007e41388e34d22c00df9
QuyDuongNgo <quyduongngo2000@gmail.com>
2024-04-10 01:36:52 +0700

    [Lesson 1][Exercise] Delete All or Delete Procedure to checked

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -11,9 +11,11 @@
                 <th scope="col" class="p-4">
                   <div class="flex items-center">
                     <input
+                      :checked="checkAll"
                       id="checkbox-all"
                       type="checkbox"
                       class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      @change="handleCheckAll"
                     />
                     <label for="checkbox-all" class="sr-only">checkbox</label>
                   </div>
@@ -33,13 +35,19 @@
             <tbody
               class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700"
             >
-              <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+              <tr
+                v-for="(task, index) in tasksList"
+                :key="index"
+                class="hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
                 <td class="p-4 w-4">
                   <div class="flex items-center">
                     <input
+                      :checked="task.checked"
                       id="checkbox-table-5"
                       type="checkbox"
                       class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      @change="handleCheckTask($event, index)"
                     />
                     <label for="checkbox-table-5" class="sr-only"
                       >checkbox</label
@@ -49,13 +57,20 @@
                 <td
                   class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  Apple Watch Series 7
+                  {{ task.name }}
                 </td>
 
                 <td
                   class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap"
                 >
-                  <a class="text-blue-600 dark:text-blue-500 hover:underline"
+                  <a
+                    class="text-blue-600 dark:text-blue-500 hover:underline cursor-pointer mx-4"
+                    @click="handleCopy(task)"
+                    >Copy</a
+                  >
+                  <a
+                    class="text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
+                    @click="handleDeleteProduct(index)"
                     >Delete</a
                   >
                 </td>
@@ -70,10 +85,34 @@
 
 <script>
 export default {
+  props: {
+    tasksList: {
+      type: Array,
+      default: () => [],
+    },
+    checkAll: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       count: 0,
     };
+  },
+  methods: {
+    handleDeleteProduct(index) {
+      this.$emit("handle-delete-product", index);
+    },
+    handleCopy(task) {
+      this.$emit("handle-copy", task);
+    },
+    handleCheckAll(event) {
+      this.$emit("hande-check-all", event.target.checked);
+    },
+    handleCheckTask(event, index) {
+      this.$emit("check-task", { checked: event.target.checked, index });
+    },
   },
 };
 </script>
