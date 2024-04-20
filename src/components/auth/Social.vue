3e29ada0479c3dd42af653b571b154fe501167dd
duckhoi-UET <khoinguyen2000bg@gmail.com>
2024-04-20 22:51:38 +0700

    update: login

--- src/components/auth/Social.vue
+++ src/components/auth/Social.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <div class="flex gap-6 mt-4">
+    <div
+      class="flex-1 flex justify-center items-center gap-4 border py-3 rounded-xl cursor-pointer hover:shadow-sm hover:outline-1"
+    >
+      <img src="@/assets/images/icon/google.png" alt="google" />
+      <span>Google</span>
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center gap-4 border py-3 rounded-xl cursor-pointer hover:shadow-sm hover:outline-1"
+    >
+      <img src="@/assets/images/icon/facebook.png" alt="facebook" />
+      <span>Facebook</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "SocialComp",
+};
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="w-[400px]">
+  <div class="min-w-[400px]">
     <h1 class="mb-6 font-bold text-2xl">Login your Account</h1>
     <LoginForm @submit="handleLogin" />
     <a-divider>Or login with</a-divider>
+    <Social />
+    <div class="mt-4 text-base">
+      Don’t have an account?
+      <router-link to="/register" class="hover:underline">Sign Up</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import LoginForm from "@/components/auth/LoginForm.vue";
+import Social from "@/components/auth/Social.vue";
 export default {
   name: "LoginPage",
   components: {
     LoginForm,
+    Social,
   },
   methods: {
     handleLogin(form) {
