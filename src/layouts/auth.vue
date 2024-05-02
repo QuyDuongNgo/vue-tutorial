a30613e7792f998d6c7e01aeda343e57e45240ee
QuyDuongNgo <quyduongngo2000@gmail.com>
2024-05-02 09:42:15 +0700

    Login

--- src/layouts/auth.vue
+++ src/layouts/auth.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="flex h-full w-full">
     <div class="bg-blue-500 flex-1 flex justify-center items-center">
-      content left
+      <div class="flex items-center justify-center fixed top-10 left-10">
+        <div class="icon">
+          <img src="@/assets/img/Exclude.png" alt="login" />
+        </div>
+        <span class="text-logo">Hiphonic</span>
+      </div>
+      <div class="flex items-center justify-center relative">
+        <img
+          class="image-1"
+          src="@/assets/img/background.png"
+          alt="background"
+        />
+        <img class="image-2 absolute" src="@/assets/img/circle.png" alt="" />
+        <img
+          class="illus"
+          src="@/assets/img/Illustration.png"
+          alt="Illustration"
+        />
+      </div>
+      <div class="content fixed">
+        <h3 class="flex items-center justify-center text-1">
+          Customizable Multipurpose Dashboard
+        </h3>
+        <p class="flex items-center justify-center text-2">
+          Everything you need in an easily customizable dashboard.
+        </p>
+      </div>
+      <div class="dot-item flex items-center justify-center">
+        <div class="dot dot-1"></div>
+        <div class="dot dot-2"></div>
+        <div class="dot dot-3"></div>
+      </div>
     </div>
     <div class="flex-1 flex justify-center items-center">
       <slot></slot>
@@ -12,3 +43,67 @@
 <script>
 export default {};
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+.icon {
+  margin-right: 14.74px;
+}
+.text-logo {
+  font-size: 27.72px;
+  font-weight: 700;
+  list-style: none;
+  font-family: "Inter", sans-serif;
+  color: #fff;
+}
+.image-1 {
+  width: 524px;
+  height: 524px;
+}
+.image-2 {
+  width: 367px;
+  height: 367px;
+}
+.illus {
+  width: 343px;
+  height: 409px;
+  position: absolute;
+  top: 20%;
+  left: 20%;
+}
+.content {
+  bottom: 10%;
+}
+.text-1 {
+  font-size: 24px;
+  font-weight: 700;
+  list-style: none;
+  font-family: "Inter", sans-serif;
+  color: #fff;
+  line-height: 30px;
+}
+.text-2 {
+  font-size: 14px;
+  font-weight: 400;
+  list-style: none;
+  font-family: "Inter", sans-serif;
+  color: #fff;
+  line-height: 22.4px;
+}
+.dot {
+  width: 5px;
+  height: 5px;
+  box-shadow: 0 0 0 2px #fff;
+  border-radius: 50%;
+}
+.dot-1 {
+  background-color: #fff;
+  margin-right: 8px;
+}
+.dot-2 {
+  margin-right: 8px;
+}
+.dot-item {
+  position: fixed;
+  bottom: 5%;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,106 @@
 <template>
-  <div>Login</div>
+  <div>
+    <h3>Sign Up for an Account</h3>
+    <a-form
+      id="components-form-demo-normal-login"
+      :form="form"
+      class="login-form"
+      @submit="handleSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'userName',
+            {
+              rules: [
+                { required: true, message: 'Please input your username!' },
+              ],
+            },
+          ]"
+          placeholder="Username"
+        >
+          <a-icon
+            slot="prefix"
+            type="user"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'email',
+            {
+              rules: [{ required: true, message: 'Please input your email!' }],
+            },
+          ]"
+          placeholder="Email"
+        >
+          <a-icon
+            slot="prefix"
+            type="mail"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'password',
+            {
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+              ],
+            },
+          ]"
+          type="password"
+          placeholder="Password"
+        >
+          <a-icon
+            slot="prefix"
+            type="lock"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" class="login-form-button">
+          Sign Up
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "normal_login" });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    },
+  },
+};
 </script>
+<style scoped>
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 404px;
+  height: 56px;
+}
+.ant-form-item {
+  margin-bottom: 0;
+}
+</style>
